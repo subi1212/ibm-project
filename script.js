97d96be79abde8c44f4512f1a62aeb9841c09839
Sweshthika Sureshkumar <80559234+subi1212@users.noreply.github.com>
2021-06-10 11:41:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-
-var interest = principal * years * rate /100;
-var year = new Date().getFullYear()+parseInt(years);
 
 function updateRate() 
 {
+	var principal = document.getElementById("principal").value;
+	var rate = document.getElementById("rate").value;
+	var years = document.getElementById("years").value;
+
+	var interest = principal * years * rate /100;
+	var year = new Date().getFullYear()+parseInt(years);
+	
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("result").innerHTML=interest;
+	
+	
+	document.getElementById("rate_val").innerHTML=rate;
+	/*
+	document.getElementById("val1").innerHTML=principal;
+	document.getElementById("val2").innerHTML=rate;
+	document.getElementById("val3").innerHTML=interest*principal;
+	document.getElementById("val4").innerHTML=year; */
+	
+	document.getElementById("val1").innerHTML="If you deposit <b>"+principal+"</b>,";
+	document.getElementById("val2").innerHTML="at an interest rate of <b>"+rate+"</b>,";
+	document.getElementById("val3").innerHTML="You will receive an amount of <b>"+interest*principal+"</b>,";
+	document.getElementById("val4").innerHTML="in the year <b>"+years+"</b>";
+	
+	
+	
+	
+	
+	
+	
 }
